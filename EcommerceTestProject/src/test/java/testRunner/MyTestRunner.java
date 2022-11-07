ac2915a2e36b9340eb926f1e4b495520e3541db7
kappa <kappa@Dharanikarthick>
2022-11-07 22:41:06 +0530

    Commit with new change

--- EcommerceTestProject/src/test/java/testRunner/MyTestRunner.java
+++ EcommerceTestProject/src/test/java/testRunner/MyTestRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 		features = { "src/test/resources/featureFile" }, 
 		glue = { "stepDefinaion", "hooks" },
-		plugin = {"pretty"}
+		plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 		
 
 )
